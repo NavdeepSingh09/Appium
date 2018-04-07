f03056e3ccc99249f902fe09b496764b21a8dc86
Navdeep Singh <37985072+NavdeepSingh09@users.noreply.github.com>
2018-04-06 22:10:19 -0400

    Update AppiumCapSet.java

--- ThrivePages/AppiumCapSet.java
+++ ThrivePages/AppiumCapSet.java
@@ -12,21 +12,21 @@ public class AppiumCapSet {
 	
 	public void setCaps() throws Exception
 	{
-		String userName = "charanjitsingh3";
-		String accessKey = "pLFxDQJCvFNijTYVqX2q";
-		DesiredCapabilities caps = new DesiredCapabilities();
+	    String userName = " ";
+	    String accessKey = " ";
+	    DesiredCapabilities caps = new DesiredCapabilities();
 	    caps.setCapability("os", "android");
 	    caps.setCapability("os_version", "7.0"); 
 	    caps.setCapability("device", "Samsung Galaxy S8");
 	    caps.setCapability("realMobile", true);
 	    caps.setCapability("browserstack.debug", "true");
 	    caps.setCapability("project","Test App");
-		caps.setCapability("build", "Unknown");
-		//caps.setCapability("name", "Login page Android");
-		caps.setCapability("browserstack.video", "true");
-		caps.setCapability("browserstack.timezone", "Toronto");
+	    caps.setCapability("build", "Unknown");
+	  //caps.setCapability("name", "Login page Android");
+	    caps.setCapability("browserstack.video", "true");
+	    caps.setCapability("browserstack.timezone", "Toronto");
 	   
-		caps.setCapability("app", "bs://69cdc2a199429f4638cc27ab611d02653a602f88");
+	    caps.setCapability("app", "Application URL");
 	    AndroidDriver<AndroidElement> driver = new AndroidDriver <AndroidElement> (new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps);
 	    
 	}
